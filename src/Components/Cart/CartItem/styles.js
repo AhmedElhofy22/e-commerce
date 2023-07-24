64d2cdfb0630288ff63a76500125a9b6f4cdb4c3
Ahmed Elhofy <ahmedelhofy8686@gmail.com>
2023-07-24 03:34:09 +0300

    adding CartItems Component and its styles

---
+++ src/Components/Cart/CartItem/styles.js
@@ -0,0 +1,18 @@
+import { styled } from "@mui/system";;
+
+export default styled(() => ({
+  media: {
+    height: 260,
+  },
+  cardContent: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  cartActions: {
+    justifyContent: 'space-between',
+  },
+  buttons: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+}));
